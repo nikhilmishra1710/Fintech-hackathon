b06244c55f36331bbe47a1c013350f39a20e31cb
Kira_sudo_coder <148410371+Kira-sudo-coder@users.noreply.github.com>
2023-11-09 00:47:34 +0530

    Update custom_image_view.dart

--- lib/widgets/custom_image_view.dart
+++ lib/widgets/custom_image_view.dart
@@ -32,69 +32,69 @@ class CustomImageView extends StatelessWidget {
 
   ///a [CustomImageView] it can be used for showing any type of images
   /// it will shows the placeholder image if image is not found on network image
-  // CustomImageView({
-  //   this.url,
-  //   this.imagePath,
-  //   this.svgPath,
-  //   this.file,
-  //   this.height,
-  //   this.width,
-  //   this.color,
-  //   this.fit,
-  //   this.alignment,
-  //   this.onTap,
-  //   this.radius,
-  //   this.margin,
-  //   this.border,
-  //   this.placeHolder = 'assets/images/image_not_found.png',
-  // });
+  CustomImageView({
+    this.url,
+    this.imagePath,
+    this.svgPath,
+    this.file,
+    this.height,
+    this.width,
+    this.color,
+    this.fit,
+    this.alignment,
+    this.onTap,
+    this.radius,
+    this.margin,
+    this.border,
+    this.placeHolder = 'assets/images/image_not_found.png',
+  });
 
-  // @override
-  // Widget build(BuildContext context) {
-  //   return alignment != null
-  //       ? Align(
-  //           alignment: alignment!,
-  //           child: _buildWidget(),
-  //         )
-  //       : _buildWidget();
-  // }
+  @override
+  Widget build(BuildContext context) {
+    return alignment != null
+        ? Align(
+            alignment: alignment!,
+            child: _buildWidget(),
+          )
+        : _buildWidget();
+  }
 
-  // Widget _buildWidget() {
-  //   return Padding(
-  //     padding: margin ?? EdgeInsets.zero,
-  //     child: InkWell(
-  //       onTap: onTap,
-  //       child: _buildCircleImage(),
-  //     ),
-  //   );
-  // }
+  Widget _buildWidget() {
+    return Padding(
+      padding: margin ?? EdgeInsets.zero,
+      child: InkWell(
+        onTap: onTap,
+        child: _buildCircleImage(),
+      ),
+    );
+  }
 
-  // ///build the image with border radius
-  // _buildCircleImage() {
-  //   if (radius != null) {
-  //     return ClipRRect(
-  //       borderRadius: radius ?? BorderRadius.zero,
-  //       child: _buildImageWithBorder(),
-  //     );
-  //   } else {
-  //     return _buildImageWithBorder();
-  //   }
-  // }
+  ///build the image with border radius
+  _buildCircleImage() {
+    if (radius != null) {
+      return ClipRRect(
+        borderRadius: radius ?? BorderRadius.zero,
+        child: _buildImageWithBorder(),
+      );
+    } else {
+      return _buildImageWithBorder();
+    }
+  }
 
-  // ///build the image with border and border radius style
-  // _buildImageWithBorder() {
-  //   if (border != null) {
-  //     return Container(
-  //       decoration: BoxDecoration(
-  //         border: border,
-  //         borderRadius: radius,
-  //       ),
-  //       child: _buildImageView(),
-  //     );
-  //   } else {
-  //     return _buildImageView();
-  //   }
-  // }
+  ///build the image with border and border radius style
+  _buildImageWithBorder() {
+    if (border != null) {
+      return Container(
+        decoration: BoxDecoration(
+          border: border,
+          borderRadius: radius,
+        ),
+        child: _buildImageView(),
+      );
+    } else {
+      return _buildImageView();
+    }
+  }
 
   Widget _buildImageView() {
     if (svgPath != null && svgPath!.isNotEmpty) {
