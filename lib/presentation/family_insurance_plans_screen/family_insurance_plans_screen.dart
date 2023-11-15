43c366b2e28704e32ef2f42b58a50c02b58c6bc3
LEVIII007 <148410371+Kira-sudo-coder@users.noreply.github.com>
2023-11-16 00:15:12 +0530

    Update family_insurance_plans_screen.dart
    damn you again master

--- lib/presentation/family_insurance_plans_screen/family_insurance_plans_screen.dart
+++ lib/presentation/family_insurance_plans_screen/family_insurance_plans_screen.dart
@@ -73,112 +73,112 @@ class FamilyInsurancePlansScreen extends StatelessWidget {
                                               "Family plans cover two or more members.",
                                               style: theme.textTheme.bodyMedium)
                                         ]))),
-//                             CustomImageView(
-//                                 imagePath:
-//                                     ImageConstant.imgRectangle1467155x368,
-//                                 height: 155.v,
-//                                 width: 368.h,
-//                                 radius: BorderRadius.vertical(
-//                                     top: Radius.circular(10.h)),
-//                                 alignment: Alignment.topCenter)
-//                           ])),
-//                       Align(
-//                           alignment: Alignment.centerLeft,
-//                           child: Padding(
-//                               padding: EdgeInsets.only(left: 30.h, top: 36.v),
-//                               child: Text("Select a Insurance Plan",
-//                                   style: CustomTextStyles
-//                                       .titleLargeBluegray900_1))),
-//                       Container(
-//                           margin: EdgeInsets.only(
-//                               left: 29.h, top: 18.v, right: 29.h),
-//                           padding: EdgeInsets.symmetric(
-//                               horizontal: 26.h, vertical: 14.v),
-//                           decoration: AppDecoration.outlineGray.copyWith(
-//                               borderRadius: BorderRadiusStyle.roundedBorder10),
-//                           child: Column(
-//                               mainAxisSize: MainAxisSize.min,
-//                               mainAxisAlignment: MainAxisAlignment.end,
-//                               children: [
-//                                 SizedBox(height: 12.v),
-//                                 CustomRadioButton(
-//                                     value: "Monthly",
-//                                     groupValue: radioGroup,
-//                                     onChange: (value) {
-//                                       radioGroup = value;
-//                                     })
-//                               ])),
-//                       Container(
-//                           margin: EdgeInsets.only(
-//                               left: 29.h, top: 19.v, right: 29.h),
-//                           padding: EdgeInsets.symmetric(
-//                               horizontal: 26.h, vertical: 13.v),
-//                           decoration: AppDecoration.outlineGray.copyWith(
-//                               borderRadius: BorderRadiusStyle.roundedBorder10),
-//                           child: Column(
-//                               mainAxisSize: MainAxisSize.min,
-//                               mainAxisAlignment: MainAxisAlignment.end,
-//                               children: [
-//                                 SizedBox(height: 12.v),
-//                                 CustomRadioButton(
-//                                     width: 314.h,
-//                                     value: "",
-//                                     groupValue: radioGroup1,
-//                                     isRightCheck: true,
-//                                     onChange: (value) {
-//                                       radioGroup1 = value;
-//                                     })
-//                               ])),
-//                       Container(
-//                           margin: EdgeInsets.only(
-//                               left: 29.h, top: 15.v, right: 29.h),
-//                           padding: EdgeInsets.symmetric(
-//                               horizontal: 26.h, vertical: 13.v),
-//                           decoration: AppDecoration.outlineGray.copyWith(
-//                               borderRadius: BorderRadiusStyle.roundedBorder10),
-//                           child: Column(
-//                               mainAxisSize: MainAxisSize.min,
-//                               mainAxisAlignment: MainAxisAlignment.end,
-//                               children: [
-//                                 SizedBox(height: 14.v),
-//                                 CustomRadioButton(
-//                                     width: 314.h,
-//                                     value: "",
-//                                     groupValue: radioGroup2,
-//                                     isRightCheck: true,
-//                                     onChange: (value) {
-//                                       radioGroup2 = value;
-//                                     })
-//                               ])),
-//                       CustomElevatedButton(
-//                           text: "Continue",
-//                           margin: EdgeInsets.only(
-//                               left: 56.h, top: 86.v, right: 55.h),
-//                           onTap: () {
-//                             onTapContinue(context);
-//                           }),
-//                       SizedBox(height: 36.v),
-//                       CustomImageView(
-//                           imagePath: ImageConstant.imgGroup7826,
-//                           height: 93.v,
-//                           width: 427.h)
-//                     ])))));
-//   }
+                            CustomImageView(
+                                imagePath:
+                                    ImageConstant.imgRectangle1467155x368,
+                                height: 155.v,
+                                width: 368.h,
+                                radius: BorderRadius.vertical(
+                                    top: Radius.circular(10.h)),
+                                alignment: Alignment.topCenter)
+                          ])),
+                      Align(
+                          alignment: Alignment.centerLeft,
+                          child: Padding(
+                              padding: EdgeInsets.only(left: 30.h, top: 36.v),
+                              child: Text("Select a Insurance Plan",
+                                  style: CustomTextStyles
+                                      .titleLargeBluegray900_1))),
+                      Container(
+                          margin: EdgeInsets.only(
+                              left: 29.h, top: 18.v, right: 29.h),
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 26.h, vertical: 14.v),
+                          decoration: AppDecoration.outlineGray.copyWith(
+                              borderRadius: BorderRadiusStyle.roundedBorder10),
+                          child: Column(
+                              mainAxisSize: MainAxisSize.min,
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: [
+                                SizedBox(height: 12.v),
+                                CustomRadioButton(
+                                    value: "Monthly",
+                                    groupValue: radioGroup,
+                                    onChange: (value) {
+                                      radioGroup = value;
+                                    })
+                              ])),
+                      Container(
+                          margin: EdgeInsets.only(
+                              left: 29.h, top: 19.v, right: 29.h),
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 26.h, vertical: 13.v),
+                          decoration: AppDecoration.outlineGray.copyWith(
+                              borderRadius: BorderRadiusStyle.roundedBorder10),
+                          child: Column(
+                              mainAxisSize: MainAxisSize.min,
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: [
+                                SizedBox(height: 12.v),
+                                CustomRadioButton(
+                                    width: 314.h,
+                                    value: "",
+                                    groupValue: radioGroup1,
+                                    isRightCheck: true,
+                                    onChange: (value) {
+                                      radioGroup1 = value;
+                                    })
+                              ])),
+                      Container(
+                          margin: EdgeInsets.only(
+                              left: 29.h, top: 15.v, right: 29.h),
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 26.h, vertical: 13.v),
+                          decoration: AppDecoration.outlineGray.copyWith(
+                              borderRadius: BorderRadiusStyle.roundedBorder10),
+                          child: Column(
+                              mainAxisSize: MainAxisSize.min,
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: [
+                                SizedBox(height: 14.v),
+                                CustomRadioButton(
+                                    width: 314.h,
+                                    value: "",
+                                    groupValue: radioGroup2,
+                                    isRightCheck: true,
+                                    onChange: (value) {
+                                      radioGroup2 = value;
+                                    })
+                              ])),
+                      CustomElevatedButton(
+                          text: "Continue",
+                          margin: EdgeInsets.only(
+                              left: 56.h, top: 86.v, right: 55.h),
+                          onTap: () {
+                            onTapContinue(context);
+                          }),
+                      SizedBox(height: 36.v),
+                      CustomImageView(
+                          imagePath: ImageConstant.imgGroup7826,
+                          height: 93.v,
+                          width: 427.h)
+                    ])))));
+  }
 
-//   /// Navigates back to the previous screen.
-//   ///
-//   /// This function takes a [BuildContext] object as a parameter, which is used
-//   /// to navigate back to the previous screen.
-//   onTapArrowleftone(BuildContext context) {
-//     Navigator.pop(context);
-//   }
+  /// Navigates back to the previous screen.
+  ///
+  /// This function takes a [BuildContext] object as a parameter, which is used
+  /// to navigate back to the previous screen.
+  onTapArrowleftone(BuildContext context) {
+    Navigator.pop(context);
+  }
 
-//   /// Navigates to the familyInsuranceInfoScreen when the action is triggered.
-//   ///
-//   /// The [BuildContext] parameter is used to build the navigation stack.
-//   /// When the action is triggered, this function uses the [Navigator] widget
-//   /// to push the named route for the familyInsuranceInfoScreen.
-//   onTapContinue(BuildContext context) {
-//     Navigator.pushNamed(context, AppRoutes.familyInsuranceInfoScreen);
-//   }
-// }
+  /// Navigates to the familyInsuranceInfoScreen when the action is triggered.
+  ///
+  /// The [BuildContext] parameter is used to build the navigation stack.
+  /// When the action is triggered, this function uses the [Navigator] widget
+  /// to push the named route for the familyInsuranceInfoScreen.
+  onTapContinue(BuildContext context) {
+    Navigator.pushNamed(context, AppRoutes.familyInsuranceInfoScreen);
+  }
+}
